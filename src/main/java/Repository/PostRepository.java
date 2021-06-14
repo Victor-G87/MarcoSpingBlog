4442812724d4a9fd1e4f6216259d12b228e156e4
Victor G Hernandez <victor.g.hernandez87@gmail.com>
2021-06-14 10:01:34 -0500

    monday

--- src/main/java/Repository/PostRepository.java
+++ src/main/java/Repository/PostRepository.java
@@ -4,5 +4,5 @@ import com.codeup.springblog.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
-
+    Post findByTitle(String ad_to_be_deleted);
 }
